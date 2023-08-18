ef097bb52891a0ef5f4d1bfd5a56df62bcca5d65
bielxrd <129539000+bielxrd@users.noreply.github.com>
2023-08-18 00:44:51 -0300

    Add files via upload

---
+++ segundaParte/calculos/exemplo1.swift
@@ -0,0 +1,27 @@
+var x: Int = 12
+var y: Int = 3
+
+
+var total: Int 
+total = x + y
+print("O resultado da Soma de \(x) + \(y) é \(total)")
+
+
+total = x - y
+print("O resultado da subtracao de \(x) - \(y) igual a \(total) ")
+
+
+total = x * y
+print("O resultado da multiplicacao entre \(x) * \(y) igual a \(total) ")
+
+
+total = x / y
+print("O resultado da divisao entre \(x) e \(y) igual a \(total) ")
+
+
+total = x % y
+print("O resultado do resto da divisao entre \(x) e \(y) igual \(total) ")
+
+
+total = x % 5
+print("O resultado do resto da divisao entre \(x) e  5 igual a \(total) ")
