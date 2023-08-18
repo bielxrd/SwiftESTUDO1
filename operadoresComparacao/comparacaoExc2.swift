ccd30236b817ae35077e18acf1989f192e2ff287
bielxrd <129539000+bielxrd@users.noreply.github.com>
2023-08-18 01:43:53 -0300

    att1

--- operadoresComparacao/comparacaoExc2.swift
+++ operadoresComparacao/comparacaoExc2.swift
@@ -1,18 +1,4 @@
-/*Agora sabemos como adicionar mais condicionais no nosso if!
 
-Vamos exercitar:
-
-Crie uma condicional que trate o seguinte cenário:
-
-Idade menor que 3 a saída deve ser “Um bebê”
-
-Idade de 3 a 10 a saída deve ser “Uma criança”
-
-Idade de 11 a 17 saída deve ser “Um adolescente”
-
-Qualquer outra idade deve ser “Um adulto”
-
-Bom treino! */
 
 let idade = 5
 
--- operadoresComparacao/comparacaoSwitch/switchExc2.swift
+++ operadoresComparacao/comparacaoSwitch/switchExc2.swift
@@ -1,16 +1,5 @@
-/*Crie um switch que trate os seguintes casos e imprima a mensagem correspondente:
 
-Porcentagem seja 0 - “Insatisfatório, não acertou nenhuma questão.”
 
-Porcentagem esteja entre 1 e 20 (Incluindo 20) - “Insatisfatório, desempenho muito baixo.”
-
-Porcentagem esteja entre 21 e 50 (excluindo 50) - “Insatisfatório, vamos estudar mais?”
-
-Porcentagem esteja entre 50 e 70 (excluindo 70) - “Foi por pouco, vamos estudar mais!”
-
-Porcentagem esteja entre 70 e 90 (excluindo 90) - “Satisfatório, você foi aprovado!”
-
-Outros casos - “Excelente, desempenho memorável!” */
 
 var porcentagem = 75
 
