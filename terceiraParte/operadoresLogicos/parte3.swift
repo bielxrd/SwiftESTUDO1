9b3dade83f4c58845d0d07b65435bd371726b485
bielxrd <129539000+bielxrd@users.noreply.github.com>
2023-08-18 00:47:51 -0300

    Add files via upload

---
+++ terceiraParte/operadoresLogicos/parte3.swift
@@ -0,0 +1,23 @@
+var resultado: Bool
+
+resultado = true || true
+
+print("O resultado de true || true é \(resultado)") 
+
+
+
+resultado = true || false
+
+print("O resultado de true || false é \(resultado)") 
+
+
+
+resultado = false || true
+
+print("O resultado de false || true é \(resultado)") 
+
+
+
+resultado = false || false
+
+print("O resultado de false || false é \(resultado)") 
---
+++ terceiraParte/operadoresLogicos/parte4.swift
@@ -0,0 +1,26 @@
+var x, y, resultado: Bool
+
+x = true
+y = false
+
+resultado = x || y
+
+print("O resultado de \(x) || \(y) é \(resultado)") 
+
+
+
+resultado = !x
+
+print("O resultado de !x é \(resultado)") 
+
+
+
+resultado = !y && x
+
+print("O resultado de !y && x é \(resultado)") 
+
+
+
+resultado = false || false
+
+print("O resultado de false && false é \(resultado)") 
