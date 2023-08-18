c04fe8757e32c6b18e79d9044d3ab4e065399e18
bielxrd <129539000+bielxrd@users.noreply.github.com>
2023-08-18 00:44:41 -0300

    Create calculos.swift

---
+++ segundaParte/calculos/calculos.swift
@@ -0,0 +1,35 @@
+
+var x1, x2 : Int
+x1 = 5
+x2 = 10
+var total : Int = x1 + x2
+
+print("A soma de " + String(x1) + " + " + String(x2) + " é "+String(total))
+
+x1 = 50
+x2 = 25
+total = x1 - x2
+
+print("A subtração de " + String(x1) + " - " + String(x2) + " é "+String(total))
+
+x1 = 5
+x2 = 5
+total = x1 * x2
+
+print("A multiplicação de " + String(x1) + " * " + String(x2) + " é "+String(total))
+
+x1 = 80
+x2 = 8
+total = x1 / x2
+
+print("A divisão de " + String(x1) + " / " + String(x2) + " é "+String(total))
+
+x1 = 4
+x2 = 2
+total = x1 % x2
+
+print("O resto da divisao entre " + String(x1) + " e " + String(x2) + " é "+String(total))
+
+
+
+print("Teste o resto da divisao entre \(x1) e \(x2) igual a \(total)")
