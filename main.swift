7a992b4ae2b7cb16a39101ac2a510f2d30ef239d
bielxrd <129539000+bielxrd@users.noreply.github.com>
2023-08-18 00:38:22 -0300

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,72 @@
+import Swift
+var nomeCompleto = "Gabriel Dias Ramos" //Variavel
+print(nomeCompleto)
+
+var saborPizza = "Calabresa" //Variavel
+print(saborPizza)
+
+let nomePizzaria = "pizzaForDevs" //Constante. Let utilizado para criar constantes, impossivel mudar valor
+let precoUnico = 49.99
+print(nomePizzaria)
+
+saborPizza = "Quatro-Queijos"
+print(saborPizza)
+
+print(type(of: precoUnico))
+
+print()
+
+/************************* 
+*******Criar 3 váriaveis Int 
+**************************/
+var num1, num2, num3 : Int
+num1 = 5
+num2 = 10
+num3 = 15
+
+var num4: Double = 5.50
+
+
+
+
+print(num1) //Imprimir os valores
+print(num2) //Imprimir os valores
+print(num3) //Imprimir os valores
+print(num4)
+
+
+/************************* 
+*******Criar 3 váriaveis String 
+**************************/
+var nomeUm, nomeDois, nomeTres : String
+nomeUm = "Cavalo"
+nomeDois = "Gabriel"
+nomeTres = "Roberto"
+
+
+print(nomeUm) //Imprimir os valores
+print(nomeDois) //Imprimir os valores
+print(nomeTres) //Imprimir os valores
+
+
+
+/************************* 
+*******Criar 2 váriaveis Bool 
+**************************/
+var verdadeiro, falso : Bool
+verdadeiro = true
+falso = false
+
+
+
+
+
+print(verdadeiro) //Imprimir os valores
+print(falso) //Imprimir os valores
+
+
+
+
+var primeiroNome = "Joao"
+var segundoNome = primeiroNome
+print(segundoNome)
