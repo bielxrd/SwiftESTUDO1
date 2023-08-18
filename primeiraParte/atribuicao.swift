649f39101fac8fb7ea74895e40f1c7981fa7e17f
bielxrd <129539000+bielxrd@users.noreply.github.com>
2023-08-18 00:41:42 -0300

    Create atribuicao.swift

---
+++ primeiraParte/atribuicao.swift
@@ -0,0 +1,44 @@
+/************************* 
+*******Criar 3 váriaveis Int 
+**************************/
+var num1, num2, num3 : Int
+num1 = 5
+num2 = 10
+num3 = 15
+
+
+
+
+print(num1) //Imprimir os valores
+print(num2) //Imprimir os valores
+print(num3) //Imprimir os valores
+
+
+/************************* 
+*******Criar 3 váriaveis String 
+**************************/
+var nomeUm, nomeDois, nomeTres : String
+nomeUm = "Cavalo"
+nomeDois = "Gabriel"
+nomeTres = "Roberto"
+
+
+print(nomeUm) //Imprimir os valores
+print(nomeDois) //Imprimir os valores
+print(nomeTres) //Imprimir os valores
+
+
+
+/************************* 
+*******Criar 2 váriaveis Bool 
+**************************/
+var verdadeiro, falso : Bool
+verdadeiro = true
+falso = false
+
+
+
+
+
+print(verdadeiro) //Imprimir os valores
+print(falso) //Imprimir os valores
